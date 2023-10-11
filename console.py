6ad125fb5dd54ca1553064f31a6c1733c7a80c63
Eniola011 <yettyagbalu@gmail.com>
2023-10-11 16:47:01 -0700

    update:console

--- console.py
+++ console.py
@@ -2,16 +2,26 @@
 """Command-Line Interpreter"""
 
 
+import json
 import cmd
+from models import storage
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """HBNBCommand"""
     prompt = "(hbnb) "
+    cmdClass = ["BaseModel"]
 
     def emptyline(self):
         pass
 
+    def do_create(self, MyModel):
+        """Creates a new instance of BaseModel"""
+        if not MyModel:
+            print("** class name missing **")
+        elif MyModel not in HBNBCommand.cmdClass:
+            print("** class doesn't exist **")
     def do_quit(self, line):
         """Quit command to exit the program"""
         return True
