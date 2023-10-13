278c098513c46a7adbf2d2924e9f6ba3a0927698
Eniola011 <yettyagbalu@gmail.com>
2023-10-13 06:12:40 -0700

    test case for basemodel

--- tests/test_models/test_engine/test_base_model.py
+++ tests/test_models/test_engine/test_base_model.py
@@ -5,25 +5,43 @@
 import unittest
 import datetime
 from models.base_model import BaseModel
+import os
+from models import storage
+from models.engine.file_storage import FileStorage
 
 
 class TestBaseModel(unittest.TestCase):
     """Test the class BaseModel"""
+    my_model = BaseModel()
 
-    def setUp(self):
-        """sets up basemodel"""
-        self.baseModel = BaseModel()
+    def testBasemodel(self):
+        """Test attributes in Base Model"""
+        self.my_model = "My First Model"
+        self.my_model.my_number = 89
+        self.my_model.save()
+        my_model_json = self.my_model.to_dict()
 
-    def testAttribute(self):
-        """test the attribute of basemodel"""
-        self.assertTrue(hasattr(self.baseModel, "created_at"))
-        self.assertFalse(hasattr(self.baseModel, "updated_at"))
-        self.assertEqual(self.baseModel.__class__.__name__, "BaseModel")
+        self.assertEqual('BaseModel', my_model_json['__class__'])
+        self.assertEqual(self.my_model.my_number, my_model_json['my_number'])
+        self.assertEqual(self.my_model.id, my_model_json['id'])
+        self.assertEqual(self.my_model.name, my_model_json['name'])
 
     def testSave(self):
         """test save method in base class"""
-        self.testModel.save()
-        self.assertTrue(hasattr(self.baseModel, "updated_at"))
+        self.my_model.first_name = "First"
+        self.my_model.save()
+        dict1 = self.my_model.to_dict()
 
-#def testStrng(self):
-        """test str method"""
+        self.assertIsInstance(self.my_model.id, str)
+        self.assertIsInstance(self.my_model.created_at, datetime.datetime)
+        self.assertIsInstance(self.my_model.updated_at, datetime.datetime)
+
+        self.my_model.first_name = "Second"
+        self.my_model.save()
+        dict2 = self.my_model.to_dict()
+        self.assertEqual(dict1['created_at'], dict2['created_at'])
+        self.assertNotEqual(dict1['updated_at'], dict2['updated_at'])
+
+
+if __name__ == '__main__':
+    unittest.main()
