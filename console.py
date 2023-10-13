0e53cf0d89e36101876b997493e094615cabb9ed
Eniola011 <yettyagbalu@gmail.com>
2023-10-13 05:08:03 -0700

    update: console

--- console.py
+++ console.py
@@ -6,6 +6,7 @@ import json
 import cmd
 from models import storage
 from models.base_model import BaseModel
+import shlex
 
 
 class HBNBCommand(cmd.Cmd):
@@ -85,6 +86,38 @@ class HBNBCommand(cmd.Cmd):
                 if objName == argmt[0]:
                     allInstance += [value.__str__()]
             print(allInstance)
+
+    def do_update(self, argmt):
+        """Updates an instance based on the class name and id by adding
+        or updating attribute (save the change into the JSON file).
+        """
+        if not argmt:
+            print("** class name missing **")
+            return
+        arg = ""
+        for i in argmt.split(','):
+            arg += i
+        argmts = shlex.split(arg)
+
+        if argmts[0] not in HBNBCommand.cmdClass:
+            print("** class doesn't exist **")
+        elif len(argmt) == 1:
+            print("** instance id missing **")
+        else:
+            obj = storage.all()
+            for key, value in obj.items():
+                objName = value.__class__.__name__
+                objId = value.id
+                if objName == argmt[0] and objId == argmt[1].strip('"'):
+                    if len(argmts) == 2:
+                        print("** attribute name missing **")
+                    elif len(argmts) == 3:
+                        print("** value missing **")
+                    else:
+                        setattr(key, argmts[2], argmts[3])
+                        storage.save()
+                    return
+            print("** no instance found **")
 
     def do_quit(self, line):
         """Quit command to exit the program"""
