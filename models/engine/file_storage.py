88856e18097f659ebf10a8ef49a309ee29a9d910
Eniola011 <yettyagbalu@gmail.com>
2023-10-15 13:04:59 -0700

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -38,8 +38,6 @@ class FileStorage():
         from models.amenity import Amenity
         from models.place import Place
         from models.review import Review
-
-
         dicty = {'BaseModel': BaseModel, 'User': User}
         if os.path.exists(FileStorage.__file_path) is True:
             with open(FileStorage.__file_path, "r", encoding="utf-8") as fd:
