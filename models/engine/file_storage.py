489c2a0d88268b429fe538c6cad68f17155e0702
Eniola011 <yettyagbalu@gmail.com>
2023-10-15 13:00:59 -0700

    updates file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -34,6 +34,11 @@ class FileStorage():
         from models.base_model import BaseModel
         from models.user import User
         from models.state import State
+		from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
 
         dicty = {'BaseModel': BaseModel, 'User': User}
         if os.path.exists(FileStorage.__file_path) is True:
